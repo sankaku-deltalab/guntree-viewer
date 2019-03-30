a64f4f5e028914a48aecab8f293b880f9a3e99ad
Sankaku <sankaku_dlt.45631@outlook.jp>
2019-03-30 15:04:19 +0900

    Update root, code, and settings

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,89 @@
 <template>
   <v-app>
-    <v-card height="100vh" flat>
-      <v-content>
-        <p v-if="activeId === 'home'">Home</p>
-        <p v-else-if="activeId === 'play'">Play</p>
-        <Code v-else-if="activeId === 'code'"></Code>
-        <Settings v-else-if="activeId === 'settings'"></Settings>
-      </v-content>
+    <v-content>
+      <p v-if="activeId === 'home'">Home</p>
+      <p v-else-if="activeId === 'play'">Play</p>
+      <TextEditor v-else-if="activeId === 'code'" key="code" :initialText="initialCode" v-on:change="updateCode"></TextEditor>
+      <TextEditor v-else-if="activeId === 'settings'" key="settings" :initialText="initialSettings" v-on:change="updateSettings"></TextEditor>
+    </v-content>
 
-      <v-bottom-nav :active.sync="activeId" :value="true" absolute color="transparent">
-        <v-btn color="teal" flat value="home">
-          <span>Home</span>
-          <v-icon>home</v-icon>
-        </v-btn>
+    <v-bottom-nav :active.sync="activeId" :value="true" app fixed color="transparent">
+      <v-btn color="teal" flat value="home">
+        <span>Home</span>
+        <v-icon>home</v-icon>
+      </v-btn>
 
-        <v-btn color="teal" flat value="play">
-          <span>Play</span>
-          <v-icon>play_arrow</v-icon>
-        </v-btn>
+      <v-btn color="teal" flat value="play">
+        <span>Play</span>
+        <v-icon>play_arrow</v-icon>
+      </v-btn>
 
-        <v-btn color="teal" flat value="code">
-          <span>Code</span>
-          <v-icon>code</v-icon>
-        </v-btn>
+      <v-btn color="teal" flat value="code">
+        <span>Code</span>
+        <v-icon>code</v-icon>
+      </v-btn>
 
-        <v-btn color="teal" flat value="settings">
-          <span>Settings</span>
-          <v-icon>settings</v-icon>
-        </v-btn>
+      <v-btn color="teal" flat value="settings">
+        <span>Settings</span>
+        <v-icon>settings</v-icon>
+      </v-btn>
 
-      </v-bottom-nav>
-    </v-card>
+    </v-bottom-nav>
   </v-app>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator';
-import Code from './components/Code.vue';
-import Settings from './components/Settings.vue';
+import TextEditor from './components/TextEditor.vue';
 
 @Component({
   components: {
-    Code,
-    Settings,
+    TextEditor,
   },
 })
 export default class App extends Vue {
   private activeId: string = 'home';
   private contentSize: string = '10vh';
+  private initialCode: string = `concat(
+  setMuzzle('center-muzzle'),
+  attachVirtualMuzzle(aimingMuzzle()),
+  repeat(
+    { times: 10, interval: 4 },
+    fire(bullet()),
+  ),
+);
+`;
+  private initialSettings: string = `[setting.enemy]
+position = { x: 50, y: 25 },  // area size: 100 x 100
+angleDeg = 90,  // clockwise, 0: right direction
 
-  public onBarSelectionChanged(activeId: string): void {
-    this.activeId = activeId;
+[[setting.enemy.muzzle]]
+name = 'center-muzzle',
+position = { x: 0, y: -5 },  // relative
+angleDeg = 0,
+
+[[setting.enemy.muzzle]]
+name = 'right-muzzle',
+position = { x: 30, y: 5 },
+angleDeg = 0,
+
+[[setting.enemy.muzzle]]
+name = 'left-muzzle',
+position = { x: -30, y: 5 },
+angleDeg = 0,
+`;
+
+  constructor() {
+    super();
   }
 
-  // public mounted() {
-  //   this.contentSize = this.getContentSize();
-  // }
+  public updateCode(newCode: string): void {
+    console.log('code updated');
+  }
 
-  // public getContentSize(): string {
-  //   const wrapper = this.$refs.wrapper as Vue | undefined;
-  //   const bottomNav = this.$refs.bottomNav as Vue;
-  //   if (wrapper === undefined) { return '100vh'; }
-  //   const height = wrapper.$el.clientHeight - bottomNav.$el.clientHeight;
-  //   return `${height}px`;
-  // }
+  public updateSettings(newSettings: string): void {
+    console.log('settings updated');
+  }
 }
 </script>
 
---
+++ src/components/TextEditor.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-textarea
+    v-model="text"
+    rows=30
+    flat
+    auto-grow
+    v-on:change="$emit('change', text)"
+  ></v-textarea>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+// TODO: Fix textarea height
+@Component({
+  components: {
+  },
+})
+export default class TextEditor extends Vue {
+  @Prop() private initialText!: string;
+  private text: string = '';
+
+  constructor() {
+    super();
+  }
+
+  public mounted() {
+    this.text = this.initialText;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
