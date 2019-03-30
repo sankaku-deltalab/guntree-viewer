e3c15aab4af3db5a9e3ba30d4571453c29d47c4b
Sankaku <sankaku_dlt.45631@outlook.jp>
2019-03-30 13:03:15 +0900

    Add Vuetify instead of vue-material

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
 import Vue from 'vue';
-import VueMaterial from 'vue-material';
-import 'vue-material/dist/vue-material.min.css';
+import './plugins/vuetify';
 
 import App from './App.vue';
-
-Vue.config.productionTip = false;
-
-Vue.use(VueMaterial);
 
 new Vue({
   render: (h) => h(App),
